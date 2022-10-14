120dc3bac75bf13ffe0ef57ededd92b6c002a63d
Jomar A. França <jomar.almeida@gmail.com>
2022-10-14 00:18:35 -0300

    Adicionando um Array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
 import React from 'react';
 import './App.css';
 
+const nomes = [
+    {
+        id: 1,
+        name: 'Jomar',
+    },
+    {
+        id: 2,
+        name: 'Marly',
+    },
+    {
+        id: 3,
+        name: 'Carlos',
+    },
+    {
+        id: 4,
+        name: 'Eloah',
+    },
+    {
+        id: 5,
+        name: 'Cadu',
+    },
+    {
+        id: 6,
+        name: 'Juca',
+    },
+    {
+        id: 7,
+        name: 'Sueli',
+    },
+    {
+        id: 8,
+        name: 'João',
+    },
+];
+
 function App() {
     return (
         <div className="App">
