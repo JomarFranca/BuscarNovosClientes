8a426943c713f8a90b3eb05d0cd1d5fb5c258d7a
Jomar A. França <jomar.almeida@gmail.com>
2022-10-16 22:27:49 -0300

    Editei o conteúdo pra filtrar

--- src/App.js
+++ src/App.js
@@ -1,47 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-const nomes = [
-    {
-        id: 1,
-        name: 'Jomar',
-    },
-    {
-        id: 2,
-        name: 'Marly',
-    },
-    {
-        id: 3,
-        name: 'Carlos',
-    },
-    {
-        id: 4,
-        name: 'Eloah',
-    },
-    {
-        id: 5,
-        name: 'Cadu',
-    },
-    {
-        id: 6,
-        name: 'Juca',
-    },
-    {
-        id: 7,
-        name: 'Sueli',
-    },
-    {
-        id: 8,
-        name: 'João',
-    },
-];
-
 function App() {
+    const cliente = [
+        {
+            id: 1,
+            name: 'Jomar',
+        },
+        {
+            id: 2,
+            name: 'Marly',
+        },
+        {
+            id: 3,
+            name: 'Carlos',
+        },
+        {
+            id: 4,
+            name: 'Eloah',
+        },
+        {
+            id: 5,
+            name: 'Cadu',
+        },
+        {
+            id: 6,
+            name: 'Juca',
+        },
+        {
+            id: 7,
+            name: 'Sueli',
+        },
+        {
+            id: 8,
+            name: 'João',
+        },
+    ];
+    const [enviado, setEnviado] = useState(false);
+    console.log(cliente);
+    document.getElementById('demo').innerHTML = cliente.map();
     return (
         <div className="App">
             <p>Buscar Novos</p>
             <input type="text" placeholder="Buscar Novos"></input>
-            <textarea rows={6}></textarea>
+            <button id="btn" className="btn" onClick={() => setEnviado(true)}>
+                Enviar
+            </button>
+            <div>
+                <p>Clientes:{enviado}</p>
+                <p id="demo"></p>
+            </div>
         </div>
     );
 }
