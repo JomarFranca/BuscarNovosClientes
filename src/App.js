d0ec2dc7e925c8b6527bd5dc6fe0004a5c5d4911
Jomar A. França <jomar.almeida@gmail.com>
2022-10-26 01:12:51 -0300

    Adicionado sistema de filtro de array

--- src/App.js
+++ src/App.js
@@ -1,59 +1,29 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-    const clientes = [
-        {
-            id: 1,
-            name: 'Jomar',
-        },
-        {
-            id: 2,
-            name: 'Marly',
-        },
-        {
-            id: 3,
-            name: 'Carlos',
-        },
-        {
-            id: 4,
-            name: 'Eloah',
-        },
-        {
-            id: 5,
-            name: 'Cadu',
-        },
-        {
-            id: 6,
-            name: 'Juca',
-        },
-        {
-            id: 7,
-            name: 'Sueli',
-        },
-        {
-            id: 8,
-            name: 'João',
-        },
-    ];
+const clientes = ['Jomar', 'Marly', 'Carlos', 'Eloah', 'Cadu', 'Juca', 'Sueli', 'João'];
 
-    console.log(clientes.name);
+function App() {
+    const [busca, setBusca] = useState('');
+    console.log(busca);
+
+    const clientesFiltrados = clientes.filter((cliente) => cliente.includes(busca));
 
     return (
         <div className="App">
-            <p>Buscar Novos</p>
-            <input type="text" placeholder="Buscar Novos"></input>
-            {/* <button id="btn" className="btn">
-                Enviar
-            </button> */}
-            <div>
-                <p>Clientes:</p>
-                <ul>
-                    {clientes.map((cliente) => (
-                        <li key={cliente}>{cliente.name}</li>
-                    ))}
-                </ul>
-            </div>
+            <p>Buscar Novos Clientes</p>
+            <input
+                type="text"
+                value={busca}
+                onChange={(ev) => setBusca(ev.target.value)}
+                placeholder="Listas de clientes"
+            ></input>
+            <p>Clientes:</p>
+            <ul>
+                {clientesFiltrados.map((cliente) => (
+                    <li key={cliente}>{cliente}</li>
+                ))}
+            </ul>
         </div>
     );
 }
